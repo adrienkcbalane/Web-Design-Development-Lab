2e306b38806cc096140e02fbc2fff5a90d50da7d
Adrien Balane <89111615+adrienkcbalane@users.noreply.github.com>
2022-09-30 11:29:53 +0800

    Add files via upload

---
+++ Balane_Exercise1.php
@@ -0,0 +1,23 @@
+<html>
+<head><title>Exercise</title></head>
+
+<?php
+echo "Hello, Good day! My name is Adrien Kyle C. Balane ";
+echo "<br>";
+echo "taking the course of B.S Computer Engineering, ";
+echo "<br>";
+echo "currently a 3rd Year Student under the section of WDD-58043, ";
+echo "<br>";
+echo "and Today, I will learn PHP Programming. ";
+echo "<h2>Adrien Kyle C. Balane </h2>";
+echo "<h2> Bachelor of Science in Computer Engineering </h2>";
+echo "<h2>3rd Year- WDD-58043</h2>";
+echo "Today, I will learn PHP Programming.";
+echo "<br>";
+echo "<br>";
+echo "<! -- --><marquee> Thank you so much!</marquee>";
+?> 
+
+</body>
+</html>
+
