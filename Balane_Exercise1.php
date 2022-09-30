f9758717a5dad7394ccf026136cb619e8bb40804
Adrien Balane <89111615+adrienkcbalane@users.noreply.github.com>
2022-09-30 21:41:47 +0800

    Add files via upload

--- Balane_Exercise1.php
+++ Balane_Exercise1.php
@@ -1,21 +1,18 @@
 <html>
-<head><title>Exercise</title></head>
-
+<head><title>Exercise 1</title></head>
 <?php
-echo "Hello, Good day! My name is Adrien Kyle C. Balane ";
-echo "<br>";
-echo "taking the course of B.S Computer Engineering, ";
-echo "<br>";
-echo "currently a 3rd Year Student under the section of WDD-58043, ";
-echo "<br>";
-echo "and Today, I will learn PHP Programming. ";
-echo "<h2>Adrien Kyle C. Balane </h2>";
-echo "<h2> Bachelor of Science in Computer Engineering </h2>";
-echo "<h2>3rd Year- WDD-58043</h2>";
-echo "Today, I will learn PHP Programming.";
-echo "<br>";
-echo "<br>";
-echo "<! -- --><marquee> Thank you so much!</marquee>";
+    $FullName = " Adrien Kyle C. Balane ";
+    echo $FullName;
+    echo "<br>";
+    $CollegeProgram = "Bachelor of Science in Computer Engineering";
+    echo $CollegeProgram;
+    echo "<br>";
+    $YearandSection = "3rd Year - WDD-58043";
+    echo $YearandSection;
+    echo "<br>";
+    echo $string = "Today, I will learn PHP Programming.";
+	echo "<br>";
+	echo "<! -- --><marquee> Thank you so much!</marquee>";
 ?> 
 
 </body>
